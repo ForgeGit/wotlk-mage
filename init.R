411b16d08409cac89eb2871fe0d2f83628d0ae23
Jorge Ruiz-Cabrejos <canalhorchatero@gmail.com>
2023-04-11 00:03:43 +0200

    yes

---
+++ init.R
@@ -0,0 +1,11 @@
+
+my_packages = c("jsonlite", "httr","dplyr","tidyr","readr","DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
+
---
+++ ui.R
@@ -0,0 +1,22 @@
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      # input for log ID
+      textInput("log_id", "Enter Log ID:"),
+      
+      actionButton("submit_log_id",icon("refresh")),
+      # input for character selection
+      selectInput("character", "Select Character:", choices = NULL),
+      # input for fight selection
+      selectInput("fight", "Select Fight:", choices = NULL),
+      
+      actionButton("submit_char_id","Estimate Ignite Metrics")
+    ),
+    mainPanel(
+      # output for data table
+      #  dataTableOutput("table"),
+      dataTableOutput("table2"),
+      htmlOutput("summary")
+    )
+  )
+)

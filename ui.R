83cd59e4d171793925b27366c3f7d1523053eb1e
Jorge Ruiz-Cabrejos <canalhorchatero@gmail.com>
2023-04-11 05:28:47 +0200

    Aesthetics

--- ui.R
+++ ui.R
@@ -4,22 +4,32 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
+      "Instructions:",
+      br(),
+      "Enter log, press ENTER button, select mage and fight, press ESTIMATE button",
+      br(),
+      "Currently limited boss support (Algalon, Thorim, Vezax and Razorscale)",
+      br(),
+      
       # input for log ID
       textInput("log_id", "Enter Log ID:"),
       
-      actionButton("submit_log_id","Enter log and load data",icon("refresh")),
+      actionButton("submit_log_id","ENTER log and load data",icon("refresh")),
       # input for character selection
       selectInput("character", "Select Character:", choices = NULL),
       # input for fight selection
       selectInput("fight", "Select Fight:", choices = NULL),
       
-      actionButton("submit_char_id","Estimate Ignite Metrics"),
-      actionButton("debug_id","DEBUG TABLE"),
+      actionButton("submit_char_id","ESTIMATE Ignite Metrics"),
+      actionButton("debug_id","debug table"),
       br(),
+      br(), 
+      span("Forge#0001 [Vivax-Pagle(US)]", style = "font-style:italic"),
       br(),
-      "_- Forge#0001 [Vivax-Pagle(US)]_",
-  #    "Consider a donation to upkeep this service:"
-      "See my other work: https://github.com/ForgeGit?tab=repositories"
+      #    "Consider a donation to upkeep this service:"
+      "See my other work: ",
+      br(),
+      "https://github.com/ForgeGit?tab=repositories"
     ),
     mainPanel(
       # output for data table

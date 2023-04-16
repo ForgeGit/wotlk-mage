a472784d3ed12b130eb73bbe284c4fcadb75157f
Jorge Ruiz-Cabrejos <canalhorchatero@gmail.com>
2023-04-16 08:48:51 +0200

    cache

--- server.R
+++ server.R
@@ -1116,7 +1116,7 @@ server <- function(input, output,session) {
                                 type="web",
                                 redirect_uris="https://wotlk-mage.herokuapp.com/")
         #drive_deauth()
-       drive_auth(email = TRUE, use_oob = TRUE,token = jsonlite::fromJSON(Sys.getenv("DRIVE_ACCOUNT")))
+       drive_auth(cache = ".secrets",email = TRUE, use_oob = TRUE, token = jsonlite::fromJSON(Sys.getenv("DRIVE_ACCOUNT")))
         gs4_auth(email=Sys.getenv("EMAIL_DRIVE"),token = a)
         #a<-gar_auth_service(json = json_string2)
         
